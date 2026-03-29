36892da812e97527d3d562ba604975d810ec890c
Luigi Gonnella <luigigonnella02@gmail.com>
2026-03-29 19:17:06 +0200

    refactor groupAnagrams implementation for improved efficiency and clarity

--- NeetCode150/1_ArraysAndHashing/49_group_anagrams/OPTIMAL_49_group_anagrams.c
+++ NeetCode150/1_ArraysAndHashing/49_group_anagrams/OPTIMAL_49_group_anagrams.c
@@ -1,144 +1,22 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-// /**
-//  * Return an array of arrays of size *returnSize.
-//  * The sizes of the arrays are returned as *returnColumnSizes array.
-//  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
-//  */
-
-
-// //like brute force
-// //time ~ O(N^2 * N_strings) with N=STRLEN
-// //memory ~ O(N²) caused by realloc, inefficient, memory fragmentation
-// typedef struct Element {
-//     int key;
-//     int counter;
-// } Element;
-
-// Element table[26];
-
-// int hash(char letter) {
-//     return (int)(letter - 'a');
-// }
-
-// void insertIncr(char letter) {
-//     int index = hash(letter);
-//     table[index].counter++;
-// }
-
-// bool decr(char letter) {
-//     int index = hash(letter);
-//     if (table[index].counter == 0) {
-//         return false;
-//     }
-//     table[index].counter--;
-
-//     return true;
-     
-// }
-
-// void initTable() {
-//     for (int i =0; i< 26; i++) {
-//         table[i].key = 'a' + i;
-//         table[i].counter = 0;
-//     }
-// }
-
-// bool isAnagram(char* s1, char*s2) {
-//     initTable();
-//     int len1 = strlen(s1);
-//     int len2 = strlen(s2);
-
-//     if (len1 != len2) {
-//         return false;
-//     }
-
-//     for (int i =0; i< len1; i++) {
-//         insertIncr(s1[i]);
-//     }
-//     for (int i =0; i< len2; i++) {
-//         if (!decr(s2[i])) return false;
-//     }
-
-//     for (int i =0; i< 26; i++) {
-//         if (table[i].counter != 0) return false;
-//     }
-
-//     return true;
-
-// }
-
-
-// char*** groupAnagrams(char** strs, int strsSize, int* returnSize, int** returnColumnSizes) {
-//     char*** output = NULL;
-//     int i, j;
-//     int bits[strsSize];
-//     //*returnColumnSizes = NULL;
-//     for (int i=0; i<strsSize; i++) {
-//         bits[i] = 0;
-//     }
-
-
-//     *returnSize = 0;
-//     for (i = 0; i<strsSize; i++) {
-//         int n_anagrams = 1;
-
-//         printf("processing string %s\n", strs[i]);
-
-//         if (bits[i] != 1) {
-//             (*returnSize)++; //add a list of anagrams
-//             output = realloc(output, (*returnSize) * sizeof(char**));
-
-//             output[(*returnSize)-1] =  NULL;
-//             output[(*returnSize)-1] = realloc(output[(*returnSize)-1], (n_anagrams)*sizeof(char*));
-
-//             //output[(*returnSize)-1][n_anagrams] = NULL;
-//             //output[(*returnSize)-1][n_anagrams] = realloc(output[(*returnSize)-1][n_anagrams], strlen(strs[i]) * sizeof(char));
-//             output[(*returnSize)-1][n_anagrams-1] = strs[i];
-//             bits[i] = 1;
-
-//             *returnColumnSizes = realloc(*returnColumnSizes, (*returnSize) * sizeof(int));
-//             //(*returnColumnSizes)[*returnSize-1] = n_anagrams;
-
-//             for (j = 0; j<strsSize; j++) {
-//                 if (i!=j) {
-//                     if (isAnagram(strs[i], strs[j])) {
-//                         printf("%s and %s are anagrams\n", strs[i],  strs[j]);
-//                         n_anagrams++;
-//                         bits[j] = 1;
-//                         output[(*returnSize) -1] = realloc(output[(*returnSize) -1], (n_anagrams)*sizeof(char*));
-//                         //output[(*returnSize) -1][n_anagrams] = realloc(output[(*returnSize) -1][n_anagrams], strlen(strs[j])*sizeof(char));
-//                         output[(*returnSize)-1][n_anagrams-1] = strs[j];
-                        
-//                     }
-//                 }
-//             }
-
-//             (*returnColumnSizes)[*returnSize-1] = n_anagrams;
-//         }
-
-        
-
-//     }
-
-//     return output;
-    
-// }
-
+#include <stdint.h>
 
 //time ~ O(m∗n) m=#strings, n=strlen()
 //space ~ O(m) hash table
 // O(m∗n) stored lists
-
+#define TABLE_SIZE 1024
 
 typedef struct Element {
-    int key; //represent a string (hashing of a counter vector)
+    int* bits; //represent a string 
     char** anagrams; //array of strings
     int n_anagrams; //actual length
-    int capacity: //allocated capacity, to double every time
+    int capacity; //allocated capacity, to double every time
+    struct Element* next;
 } Element;
+
+Element* table[TABLE_SIZE];
 
 
 int* getBits() {
@@ -150,34 +28,116 @@ int* getBits() {
     return bits;
 }
 
-int* hash(char* str) {
+int* getBitsFilled(char* str) {
     int* bits = getBits();
     for (int i =0; i< strlen(str); i++) {
         bits[str[i] - 'a']++;
     }
-    
     return bits;
 }
 
-void insert(char* str) {
-    int index = hash(str);
-    table[index].bits++;
+int hash(char* str, int* bits) {
+    uint32_t hash = 2166136261u;
+
+    for (int i = 0; i< 26; i++) {
+        hash ^= (uint32_t) bits[i];
+        hash *= 16777619;
+    }
+    
+    return hash % TABLE_SIZE;
 }
 
-bool decr(char letter) {
-    int index = hash(letter);
-    if (table[index].counter == 0) {
+bool areEqual(int* bits1, int* bits2) {
+    if (bits1 == NULL || bits2 == NULL) {
         return false;
     }
-    table[index].counter--;
+
+    for (int i = 0; i< 26; i++) {
+        if (bits1[i] != bits2[i]) {
+            return false;
+        }
+    }
 
     return true;
-     
 }
 
-void initTable() {
-    for (int i =0; i< 26; i++) {
-        table[i].key = 'a' + i;
-        table[i].counter = 0;
+
+void insert(char* str) {
+    int* bits = getBitsFilled(str);
+    int index = hash(str, bits);
+
+    Element* curr = table[index];
+    while (curr != NULL) {
+        if (areEqual(bits, curr-> bits)) {
+            curr -> n_anagrams++;
+
+            if (curr -> n_anagrams > curr -> capacity) {
+                curr -> capacity *= 2;
+                curr -> anagrams = realloc(curr -> anagrams, curr -> capacity * sizeof(char*));
+            }
+
+            curr -> anagrams[(curr -> n_anagrams) - 1] = str;
+
+            return;
+        }
+
+        curr = curr -> next;
     }
+
+    Element* el = malloc(sizeof(Element));
+    el -> bits = bits;
+    el -> capacity = 2;
+    el -> anagrams = malloc(2 * sizeof(char*));
+    el -> anagrams[0] = str;
+    el -> n_anagrams = 1;
+    el -> next = table[index];
+    table[index] = el;
+
+
+}
+
+
+
+void initTable() {
+    for (int i =0; i< TABLE_SIZE; i++) {
+        table[i] = NULL;
+    }
+}
+
+
+char*** groupAnagrams(char** strs, int strsSize, int* returnSize, int** returnColumnSizes) {
+    char*** ret = malloc(strsSize * sizeof(char**));
+    for (int i = 0; i< strsSize; i++) {
+        ret[i] = NULL;
+    }
+
+    initTable();
+
+    for (int i = 0; i<strsSize; i++ ) {
+        insert(strs[i]);
+    }
+
+    (*returnSize) = 0;
+    int* cols = malloc(strsSize * sizeof(int));
+    int j = 0;
+    int tot = 0;
+    for (int i =0; i < TABLE_SIZE && tot < strsSize; i++) {
+        Element* el = table[i];
+        while (el != NULL) {
+            cols[j++] = el -> n_anagrams;
+            ret[(*returnSize) ++] = el -> anagrams;
+            tot += el -> n_anagrams;
+
+            el = el -> next;
+            
+        }
+    }
+   
+
+    cols = realloc(cols, j * sizeof(int));
+    (*returnColumnSizes) = cols;
+
+    ret = realloc(ret, (*returnSize) * sizeof(char**));
+
+    return ret;
 }
