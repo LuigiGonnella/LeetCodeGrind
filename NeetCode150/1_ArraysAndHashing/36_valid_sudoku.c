1a3704f1e89ec59f12193da46688d641e3a0dcaf
Luigi Gonnella <luigigonnella02@gmail.com>
2026-04-07 19:48:44 +0200

    add necessary includes for valid Sudoku implementation

--- NeetCode150/1_ArraysAndHashing/36_valid_sudoku.c
+++ NeetCode150/1_ArraysAndHashing/36_valid_sudoku.c
@@ -1,4 +1,8 @@
 #define DIM 9
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 bool checkRow(char** board, int row, int col, int occurs[]) {
     if (col >= DIM) {
