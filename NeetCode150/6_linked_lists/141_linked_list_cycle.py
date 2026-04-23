f592274b32dd687a53bd5bb2d32d31761885f22a
Luigi Gonnella <luigigonnella02@gmail.com>
2026-04-23 13:02:05 +0200

    Add implementation for detecting a cycle in a linked list using Floyd's Tortoise and Hare algorithm

---
+++ NeetCode150/6_linked_lists/141_linked_list_cycle.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast, slow = head, head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+            if fast == slow:
+                return True
+
+
+        
+        return False
+
+
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+#         uniques = set()
+
+#         curr = head
+
+#         while curr:
+#             if curr in uniques:
+#                 return True
+
+#             uniques.add(curr)
+
+#             curr = curr.next
+        
+#         return False
+
+        
