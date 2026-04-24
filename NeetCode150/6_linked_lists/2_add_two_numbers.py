eee9ff7f0c9c914ffeadf3130e2ce04ad882934d
Luigi Gonnella <luigigonnella02@gmail.com>
2026-04-24 17:16:18 +0200

    Add time and space complexity comments for addTwoNumbers function

--- NeetCode150/6_linked_lists/2_add_two_numbers.py
+++ NeetCode150/6_linked_lists/2_add_two_numbers.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.next = next
 
+#!O(N) time and O(1) extra space (O(N) for output)
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
@@ -28,4 +29,31 @@ class Solution:
             
         
         return dummy.next
+
+#O(N) time and O(N) extra space (O(2*N) for output + stack)
+# class Solution:
+#     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+#         def addR(head: Optional[ListNode], l1: Optional[ListNode], l2: Optional[ListNode], carry: int):
+#             if l1 is None and l2 is None and not carry:
+#                 return 
+            
+#             val1 = l1.val if l1 else 0
+#             val2 = l2.val if l2 else 0
+#             val = val1 + val2 + carry
+#             carry = val // 10
+
+#             val = val % 10
+
+#             head.next = ListNode(val)
+
+#             return addR(head.next, l1.next if l1 else None, l2.next if l2 else None, carry)
+
+
+
+            
+#         dummy = ListNode()
+#         addR(dummy, l1, l2, 0)
+
+#         return dummy.next
         
