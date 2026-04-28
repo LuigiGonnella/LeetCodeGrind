057d8b4cebdea3ab781a148e81153d7e631b6e6c
Luigi Gonnella <luigigonnella02@gmail.com>
2026-04-28 10:38:23 +0200

    Add implementation for finding the lowest common ancestor of two nodes in a binary search tree using recursive DFS

---
+++ NeetCode150/7_trees/235_LCA_in_BST.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+#!RECURSIVE DFS --> O(h) time and O(h) space (worst case is degenerated tree --> O(N) in both time and space)
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not p or not q:
+            return None
+        
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        
+        if p.val == root.val or q.val == root.val or p.val > root.val or q.val > root.val:
+            return root
+
+        return self.lowestCommonAncestor(root.left, p, q)
