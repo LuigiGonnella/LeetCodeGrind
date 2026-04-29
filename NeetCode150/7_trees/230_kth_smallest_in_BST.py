c4b6854b5879f37401e100e7f46c67da1fb53d52
Luigi Gonnella <luigigonnella02@gmail.com>
2026-04-29 18:14:46 +0200

    Add implementation for finding the k-th smallest element in a binary search tree using recursive and iterative DFS approaches

---
+++ NeetCode150/7_trees/230_kth_smallest_in_BST.py
@@ -0,0 +1,77 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+#!RECURSIVE DFS --> O(h + k) time (h in depth + k in reverse) but O(N) worst case. O(N) space
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        self.res = None
+
+        def dfs(root: Optional[TreeNode], k: int) -> int:
+            if not root or self.res:
+                return 0
+            
+            l = dfs(root.left, k)
+
+            if l + 1 == k:
+                self.res = root.val
+
+            r = dfs(root.right, k - l - 1)
+
+            return l + r + 1
+
+
+        dfs(root, k)
+        return self.res
+        
+
+#!RECURSIVE DFS --> O(h + k) time (h in depth + k in reverse) but O(N) worst case. O(N) space
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        self.count = k
+        self.res = None
+
+        def dfs(root: Optional[TreeNode]) -> None:
+            if not root or self.res:
+                return
+            
+            dfs(root.left) #search in left
+
+            self.count -= 1
+            if self.count == 0: #if k-th smallest
+                self.res = root.val #return
+
+            dfs(root.right) #go right
+
+        dfs(root)
+
+        return self.res
+
+#!ITERATIVE DFS --> O(h + k) time (h in depth + k in reverse) but O(N) worst case. O(N) space
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        curr = root
+
+        while stack or curr:  
+            while curr:
+                    stack.append(curr)                   
+                    curr = curr.left
+            
+            curr = stack.pop()
+            k -= 1
+            if k == 0:
+                return curr.val
+            
+            curr = curr.right
+                
+
+
+
+            
+
+
+        
