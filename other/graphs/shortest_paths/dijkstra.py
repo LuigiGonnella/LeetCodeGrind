044fa777ec572814489c24874baa26c4de2ceb0c
Luigi Gonnella <luigigonnella02@gmail.com>
2026-05-15 19:09:45 +0200

    Implement Dijkstra's algorithm for finding the shortest path in a graph

---
+++ other/graphs/shortest_paths/dijkstra.py
@@ -0,0 +1,38 @@
+
+
+#!O(ElogE) = O(ElogV)
+class Solution:
+    def dijsktra(self, n, edges, src): #find minimum distance from SRC to any other node
+
+        # 1. Build the Adjacency List
+        graph = defaultdict(list)
+
+        for u, v in edges:
+            graph[u].append(v) #directed
+        
+        # 2. Initialize Distance and Spanning Tree/Parent arrays
+        distances = [float("+inf") for _ in range(n)]
+        st = [-1 for _ in range(n)]
+
+        distances[src] = 0
+        st[src] = src 
+
+        min_heap = [(0, src)]
+
+        while min_heap: #maximum E iterations
+
+            distance, node = heapq.heapop(min_heap) #!O(logE)
+
+            # OPTIMIZATION (Lazy Deletion):
+            if distance > distances[node]: #old entry
+                continue
+
+            for neigh in graph[node]:
+                # Relaxation Step: Have we found a strictly shorter path to neigh?
+                if distances[node] + distance < distances[neigh]:
+                    distances[neigh] = distances[node] + distance
+                    parents[neigh] = node
+                    # Push the newly found better distance to the heap
+                    heapq.heappush(min_heap, (distances[neigh], neigh)) #!O(logE)
+        
+        return distance, st
