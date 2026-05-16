cc9df7ea6e9ac0ef5cb08d5c41ac98cbd4babf28
Luigi Gonnella <luigigonnella02@gmail.com>
2026-05-16 09:38:15 +0200

    Refactor Euler path function to use Hierholzer's algorithm and improve clarity

--- other/graphs/eulero_path.py
+++ other/graphs/eulero_path.py
@@ -1,29 +1,51 @@
-path = []
-visited = set()
-E = 10
+from collections import defaultdict
 
-def find_eulero_path(graph, nodeSource, nodeDest):
-
-    path.append(nodeSource)
+#!simple case (no multigraph) --> O(E!)
+def find_eulero_path(graph, start_node):
+    # 'graph' should be a dictionary of lists: { u: [v1, v2, ...] }
+    path = []
     
-
-    if nodeSource == nodeDest and len(path) == E + 1:
-        return True
-    
-    for node in graph.get(nodeSource, []):  #adjacency list in dict
-        if (nodeSource, node) not in visited:
-            visited.add((nodeSource, node))
-            if find_eulero_path(graph, node, nodeDest):
-                return True
-            visited.remove((nodeSource, node)) #backtrack
+    def dfs(node):
+        # As long as there are unused outgoing edges from this node
+        while graph[node]:
+            # Pop the edge so it is permanently removed (solves the duplicate edge issue!)
+            next_node = graph[node].pop()
+            dfs(next_node)
             
+        # We only append to the path when we are completely stuck
+        path.append(node)
+        
+    dfs(start_node)
     
-    path.pop() #backtrack
-    
-    
-    return False
+    # Hierholzer's builds the path in post-order, so we must reverse it
+    return path[::-1]
 
+# Example Usage:
+# graph = { 'A': ['B', 'C'], 'B': ['A'], 'C': [] }
+# final_path = find_eulero_path(graph, 'A')
 
-#in main:
-#if find_eulero_path(graph, nodeA, nodeB):
-    return path
+#!GENERAEL OPTIMAL CASE --> HIERHOLZER'S ALGORITHM --> O(E)
+from collections import defaultdict
+
+def find_eulero_path(graph, start_node):
+    # 'graph' should be a dictionary of lists: { u: [v1, v2, ...] }
+    path = []
+    
+    def dfs(node):
+        # As long as there are unused outgoing edges from this node
+        while graph[node]:
+            # Pop the edge so it is permanently removed (solves the duplicate edge issue!)
+            next_node = graph[node].pop()
+            dfs(next_node)
+            
+        # We only append to the path when we are completely stuck
+        path.append(node)
+        
+    dfs(start_node)
+    
+    # Hierholzer's builds the path in post-order, so we must reverse it
+    return path[::-1]
+
+# Example Usage:
+# graph = { 'A': ['B', 'C'], 'B': ['A'], 'C': [] }
+# final_path = find_eulero_path(graph, 'A')
