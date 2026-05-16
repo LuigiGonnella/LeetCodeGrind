6b58cb22b2d330d2576fadad177fe5fe904bc42e
Luigi Gonnella <luigigonnella02@gmail.com>
2026-05-16 11:02:24 +0200

    Add Prim's algorithm implementation and improve complexity comments

--- other/graphs/minimum_spanning_trees/prim.py
+++ other/graphs/minimum_spanning_trees/prim.py
@@ -1,5 +1,5 @@
 
-#!O(ElogE) = O(ElogV)
+#!O(ElogE) = O(ElogV, but for dense graphs this is O(V^2logV), so it is better the matrix solution --> O(V^2)
 class Solution:
     def minimumSpanningTree(self, n, edges):
 
@@ -39,3 +39,42 @@ class Solution:
 
         # Step 5: Verify the tree
         return min_weight if len(visited) == n else -1
+
+
+#!O(V^2)
+class Solution:
+    # Strict O(V^2) Time | O(V) Space
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        
+        #INITIALIZATION
+        n = len(points)
+
+        distances = [float("+inf")] * n
+        visited = [False] * n
+        distances[0] = 0
+        tot_cost = 0
+       
+        #INCLUDE EVERY NODE       
+        for _ in range(n):
+
+            curr_node = -1
+            min_edge = float("+inf")
+
+            #FIND THE NEAREST TO THE LAST INSERTED NODE
+            for node in range(n):
+                if not visited[node] and distances[node] < min_edge:
+                    curr_node = node
+                    min_edge = distances[node]
+            
+            #ADD IT TO THE SOLUTION
+            visited[curr_node] = True
+            tot_cost += min_edge
+
+            #UPDATE ALL DISTANCES OF ANY OTHER (NOT ALREADY VISITED) NODE
+            for next_node in range(n):
+
+                if not visited[next_node]:
+                    weight = abs(points[curr_node][0] - points[next_node][0]) + abs(points[curr_node][1] - points[next_node][1])
+                    distances[next_node] = min(distances[next_node], weight)
+        
+        return tot_cost
